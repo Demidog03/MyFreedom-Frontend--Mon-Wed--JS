c99c5e5b954737bb4a28131de5258f840968d931
Demidog03 <olzhasktl362@gmail.com>
2024-12-02 21:59:59 +0500

    1 - Основы JS (задачи)

--- 1 - Основы JS/урок/script.js
+++ 1 - Основы JS/урок/script.js
@@ -229,15 +229,198 @@ console.log(phoneNumber)
 //     console.log('Вы нам не подходите. Мы вам перезвоним)')
 // }
 
-const gender = prompt('Какой у вас пол?')
-const age = prompt('Сколько вам лет?')
-const height = prompt('Какой у вас рост?')
-const weight = prompt('Какой у вас вес?')
-const hairColor = prompt('Какой у вас цвет волос?')
+// const gender = prompt('Какой у вас пол?')
+// const age = prompt('Сколько вам лет?')
+// const height = prompt('Какой у вас рост?')
+// const weight = prompt('Какой у вас вес?')
+// const hairColor = prompt('Какой у вас цвет волос?')
 
-if (gender === 'Женщина' && age >= 20 && height >= 175 && weight <= 60 && hairColor === 'Коричневый') {
-    console.log('Поздравляю, вы модель!!!')
+// if (gender === 'Женщина' && age >= 20 && height >= 175 && weight <= 60 && hairColor === 'Коричневый') {
+//     console.log('Поздравляю, вы модель!!!')
+// }
+// else {
+//     console.log('Вы не модель!')
+// }
+
+
+// ЗАДАЧА 1
+
+// Запросите у пользователя его имя.
+// Выведите в окошке (с помощью функции alert)
+// фразу: Привет, Николай! (вместо Николай должно
+//     показываться имя, которое ввел пользователь)
+
+// const name = prompt('Введите ваше имя:')
+// // alert('Привет, ' + name + '!')
+// alert(`Привет, ${name}!`)
+
+// ЗАДАЧА 2
+// Запросите у пользователя число. Затем запросите степень,
+// в которую нужно возвести это число. Выведите в консоль 
+// результат.
+
+// +"5" => 5
+// Number("5") => 5
+// const num1 = +prompt('Введите число:')
+// const num2 = +prompt('Введите степень:')
+// console.log(`Результат: ${num1 ** num2}`)
+// console.log(Math.pow(num1, num2))
+
+
+// let age = 35
+// age = age + 1 // 35 + 1 = 36
+// age += 1
+// age -= 1
+// age *= 1
+// age /= 1
+
+// age++ // age += 1 -> age = age + 1
+// age-- // age -= 1 -> age = age - 1
+
+// age = age + 5
+// age += 5
+
+
+// a = a + 3 
+// a += 3
+
+
+// "my text" -> "some text"
+// "some text" -> "another text"
+// "fdasfdsfds" -> "some text"
+// "f43we6ethdf" -> "some text"
+
+
+// ЗАДАНИЕ 4
+// "5" == 5 -> true
+// "5" === 5 -> false
+
+// let text = "some text"
+// if (text === "some text") { // "some text" === "some text"
+//     text = "another text"
+// }
+// else {
+//     text = "some text"
+// }
+
+// console.log(text)
+
+// if () {
+
+// }
+// else if() {
+
+// }
+// else if() {
+
+// }
+// else {
+
+// }
+
+// ЗАДАНИЕ 5
+// let num1 = 0
+// if(num1 === 0) { 
+//     num1 = 1
+// }
+// else if(num1 < 0) {
+//     num1 = "less than zero"
+// }
+// else if(num1 > 0) {
+//     // num1 = num1 * 10
+//     num1 *= 10
+// }
+
+// console.log(num1)
+
+// const num1 = 5
+// if(num1 === 5) { // 5 === 5
+//     console.log('Первое условие работает')
+// }
+// if(num1 > 0) { // 5 > 0
+//     console.log('Второе условие работает')
+// }
+// else {
+//     console.log('Третье условие работает')
+// }
+
+
+// ЗАДАНИЕ 6
+
+// const num = +prompt('Введите число:')
+// let result
+
+// if(num < 5) { // 5 < 5
+//     result = "0"
+// }
+// else {
+//     result = '1'
+// }
+
+// console.log(result)
+
+
+// ЗАДАНИЕ 7
+// const num1 = +prompt('Введите первое число:')
+// const num2 = +prompt('Введите второе число:')
+
+// if(num1 > num2) { // 4 > 4
+//     console.log(`Большее число: ${num1}`)
+// }
+// else if (num2 > num1) { // 4 > 4
+//     console.log(`Большее число: ${num2}`)
+// }
+// else {
+//     console.log('Числа равны')
+// }
+
+
+// %
+// ЗАДАНИЕ 8
+// const num1 = +prompt('Введите первое число:')
+// const num2 = +prompt('Введите второе число:')
+// const isMultiple = num1 % num2 === 0 // 4 и 2 -> 4 % 2 = 0
+
+// if (isMultiple) {
+//     console.log(`Число ${num1} кратная к числу ${num2}`)
+// }
+// else {
+//     console.log(`Число ${num1} не кратная к числу ${num2}`)
+// }
+
+
+// &&
+// ЗАДАНИЕ 9
+// const mark = +prompt('Введите ваш средний балл:')
+// if(mark >= 1 && mark <= 4) {
+//     console.log('Двоечник, иди учись!')
+// }
+// else if (mark >= 5 && mark <= 8) {
+//     console.log('Неплохо, но давай еще поднажмем!')
+// }
+// else if(mark >= 9 && mark <= 10) {
+//     console.log('Ого, да ты настоящий отличник!')
+// }
+// else {
+//     console.log('Данные некорректны! Пожалуйста введите число от 0 до 10')
+// }
+
+// ЗАДАНИЕ 10
+const mark = +prompt('Введите балл за экзамен (от 0 до 100):')
+const projectCount = +prompt('Введите количество выполненных проектов (от 0 и больше)')
+let result
+
+if(mark >= 90 || projectCount >= 10) {
+    result = 100
+}
+else if(mark >= 75 && projectCount >= 5) {
+    result = 90
+}
+else if(mark >= 50 && projectCount >= 2) {
+    result = 75
 }
 else {
-    console.log('Вы не модель!')
+    result = 0
 }
+
+console.log('Итоговая оценка: ' + result)
