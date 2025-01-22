49202353d3c39d6ef367cfe2247219f1932d4d2b
Demidog03 <olzhasktl362@gmail.com>
2025-01-22 22:02:55 +0500

    7. BOM и DOM

--- 7. BOM и DOM/script.js
+++ 7. BOM и DOM/script.js
@@ -77,7 +77,7 @@
 // const text1 = document.getElementById('text1')
 // text1.remove()
 
-const forRemoveTags1 = document.querySelectorAll('.forRemove')
+// const forRemoveTags1 = document.querySelectorAll('.forRemove')
 // const forRemoveTags2 = document.getElementsByClassName('forRemove')
 
 // for (const tag of forRemoveTags1) {
@@ -91,3 +91,135 @@ const forRemoveTags1 = document.querySelectorAll('.forRemove')
 // forRemoveTags1.forEach(tag => {
 //     tag.remove()
 // })
+
+
+// Создание DOM
+// const h1Tag = document.createElement('h1')
+// h1Tag.innerText = 'MyFreedom'
+// h1Tag.style.color = 'blue'
+// document.body.appendChild(h1Tag)
+
+// const container = document.querySelector('#container')
+// const aTag = document.createElement('a')
+// aTag.innerText = 'Ссылка'
+// aTag.style.textDecoration = 'none'
+// aTag.href = 'https://google.com'
+// container.appendChild(aTag) // добавить один элемент
+
+
+// const liTags = []
+// for(let i = 0; i < 5; i++) {
+//     const liTag = document.createElement('li')
+//     liTag.innerText = `Элемент ${i}`
+//     liTags.push(liTag)
+// }
+// console.log(liTags)
+// container.append(...liTags) // добавить несколько элементов
+//  // li, li, li, li, li -> spread оператор
+
+
+// ЗАДАНИЕ 4
+// const pTag = document.createElement('p')
+// pTag.innerText = 'Some text'
+// pTag.style.fontSize = '36px'
+// pTag.style.fontWeight = 'bold'
+// document.body.appendChild(pTag)
+
+// ЗАДАНИЕ 5
+// function tagCreator(tagName, color, content) {
+//     const newTag = document.createElement(tagName)
+//     newTag.innerHTML = content
+//     newTag.style.color = color
+
+//     return newTag
+// }
+
+// const h1Tag = tagCreator('h1', 'green', 'Hello!')
+// const pTag = tagCreator('p', 'pink', 'Hello, World!')
+// const ulTag = tagCreator('ul', 'red', '<li>Olzhas</li><li>Erbol</li>')
+
+// // document.body.appendChild(h1Tag)
+// // document.body.appendChild(pTag)
+// // document.body.appendChild(ulTag)
+
+// document.body.append(h1Tag, pTag, ulTag)
+
+
+// innerHTML -> добавить текст и тэги
+// innerText -> добавить просто текст
+
+// ЗАДАНИЕ 6
+// const selectTag = document.createElement('select')
+
+// for(let i = 1960; i <= 2020; i++) {
+//     const optionTag = document.createElement('option')
+//     optionTag.innerText = i
+//     optionTag.value = i
+//     selectTag.appendChild(optionTag)
+// }
+
+// document.body.appendChild(selectTag)
+
+// ЗАДАНИЕ 7
+// const clients = [
+//     {name: "Женя", order: true},
+//     {name: "Кристина", order: true},
+//     {name: "Павел", order: false},
+//     {name: "Виолетта", order: false},
+//     {name: "Костя", order: true}
+// ]
+
+// const ulTag = document.createElement('ul')
+
+// for (const client of clients) {
+//     const liTag = document.createElement('li')
+//     liTag.innerText = `Клиент ${client.name} ${client.order ? 'оплатил' : 'отменил'} заказ`
+//     ulTag.appendChild(liTag)
+// }
+
+// document.body.appendChild(ulTag)
+
+
+// ЗАДАНИЕ 8
+// let linksArr = ['https://www.amazon.com/', 'https://www.youtube.com/', 'https://devby.io/', 'https://www.google.com/', 'https://apple.com/']
+
+// const divTag = document.createElement('div')
+// divTag.style.backgroundColor = '#ffcec3'
+// divTag.style.padding = '20px'
+
+// for (const link of linksArr) {
+//     const aTag = document.createElement('a')
+//     aTag.href = link
+//     aTag.innerText = link
+//     aTag.style.display = 'block'
+//     // aTag.target = '_blank'
+//     aTag.setAttribute('target', '_blank')
+//     divTag.appendChild(aTag)
+// }
+
+// document.body.appendChild(divTag)
+
+
+// Работа с классами
+// const openBtn = document.querySelector('#openBtn')
+// const closeBtn = document.querySelector('#closeBtn')
+// const toggleBtn = document.querySelector('#toggleBtn')
+// const list = document.querySelector('#list')
+
+// closeBtn.onclick = () => {
+//     list.classList.add('hidden')
+// }
+
+// openBtn.onclick = () => {
+//     list.classList.remove('hidden')
+// }
+
+// toggleBtn.onclick = () => {
+//     list.classList.toggle('hidden')
+//     if(list.classList.contains('hidden')) {
+//         toggleBtn.innerText = 'Открыть список'
+//     }
+//     else {
+//         toggleBtn.innerText = 'Закрыть список'
+//     }
+// }
