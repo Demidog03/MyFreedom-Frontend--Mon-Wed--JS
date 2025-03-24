b04d7286725594716e586791ebb7593b8bbc67f8
Demidog03 <olzhasktl362@gmail.com>
2025-03-24 19:14:42 +0500

    20. ООП. Принципы

--- 19. ООП. Классы, наследование классов/script.js
+++ 19. ООП. Классы, наследование классов/script.js
@@ -217,32 +217,156 @@
 
 
 // Polymorphism - вы не понимаете, это другое
-class FrontendDeveloper {
-    constructor(name, framework) {
-        this.name = name
-        this.framework = framework
+// class FrontendDeveloper {
+//     constructor(name, framework) {
+//         this.name = name
+//         this.framework = framework
+//     }
+
+//     getInfo() {
+//         console.log(`${this.name} is a frontend developer, who uses ${this.framework} framework!`)
+//     }
+
+//     work() {
+//         console.log(`${this.name} is coding layout and functionality of website!`)
+//     }
+// }
+
+// class MiddleFrontendDeveloper extends FrontendDeveloper {
+//     work() {
+//         super.work()
+//         console.log(`${this.name} is working with complex components and make unit tests!`)
+//     }
+// }
+
+// const frontendDeveloper1 = new FrontendDeveloper('Ainagul', 'Next.js')
+// frontendDeveloper1.getInfo()
+// frontendDeveloper1.work()
+
+// const frontendDeveloper2 = new MiddleFrontendDeveloper('Galymzhan', 'Angular')
+// frontendDeveloper2.getInfo()
+// frontendDeveloper2.work()
+
+
+// INCAPSULATION - кому то можно кому то нет
+// class KaspiGold {
+//     // #balance = 0 // private
+//     _balance = 0 // protected
+//     constructor(owner) {
+//         this.owner = owner
+//     }
+
+//     getSalary(company, amount) {
+//         this._balance += amount
+//         console.log(`Компания ${company} перевела ${amount} kzt на баланс. Комментарий: ЗП`)
+//     }
+// }
+
+// class KaspiDeposit extends KaspiGold {
+//     _deposit = 0
+
+//     addDeposit(amount) {
+//         this._balance -= amount
+//         this._deposit += amount
+//     }
+// }
+
+// const kaspiGoldOwner1 = new KaspiGold('Ainagul')
+// kaspiGoldOwner1.getSalary('ИП NomadCosmetology', 900000)
+// console.log(kaspiGoldOwner1)
+
+// const kaspiGoldOwner2 = new KaspiDeposit('Galymzhan')
+// kaspiGoldOwner2.addDeposit(1000000)
+// console.log(kaspiGoldOwner2)
+
+
+// ЗАДАНИЕ 4
+class Logo {
+    constructor(url) {
+      this.top = 0;
+      this.left = 0;
+      this.width = 200;
+      this.imgUrl = url;
+      this.html = null;
+    }
+  
+    init() {
+      const img = document.createElement('img') // <img/>
+      img.src = this.imgUrl // <img src="ссылка" />
+      this.html = img
+      this.render()
+  
+      document.body.style.backgroundColor = 'black'
+    }
+    
+    render() {
+      document.body.appendChild(this.html) // <body><img src="ссылка" /></body>
+      this.html.style.position = 'fixed' // <body><img src="ссылка" style="position: fixed" /></body>
+      this.html.style.top = this.top + 'px' // <body><img src="ссылка" style="position: fixed; top: 0" /></body>
+      this.html.style.left = this.left + 'px' // <body><img src="ссылка" style="position: fixed; top: 0; left: 0" /></body>
+      this.html.style.width = this.width + 'px' // <body><img src="ссылка" style="position: fixed; top: 0; left: 0; width: 200px" /></body>
+      this.html.style.zIndex = 1
+    }
+  
+    moveUp() {
+      this.top -= 20
+      this.render()
+    }
+    moveDown() {
+      this.top += 20
+      this.render()
+    }
+    moveLeft() {
+      this.left -= 20
+      this.render()
+    }
+    moveRight() {
+      this.left += 20
+      this.render()
+    }
+  }
+
+const logo1 = new Logo('https://optim.tildacdn.biz/tild6463-3261-4630-b266-343730396462/-/resize/240x/-/format/webp/logofull.png')
+logo1.init()
+logo1.moveRight()
+logo1.moveRight()
+logo1.moveRight()
+logo1.moveRight()
+logo1.moveDown()
+logo1.moveDown()
+logo1.moveDown()
+
+console.log(logo1)
+
+class Circle {
+    constructor(size, color) {
+        this.size = size
+        this.color = color
     }
 
-    getInfo() {
-        console.log(`${this.name} is a frontend developer, who uses ${this.framework} framework!`)
-    }
-
-    work() {
-        console.log(`${this.name} is coding layout and functionality of website!`)
+    render() {
+        const div = document.createElement('div')
+        div.style.width = this.size + 'px'
+        div.style.height = this.size + 'px'
+        div.style.position = 'fixed'
+        div.style.left = '0px'
+        div.style.right = '0px'
+        div.style.backgroundColor = this.color
+        div.style.borderRadius = '50%'
+        document.body.appendChild(div)
     }
 }
 
-class MiddleFrontendDeveloper extends FrontendDeveloper {
-    work() {
-        super.work()
-        console.log(`${this.name} is working with complex components and make unit tests!`)
-    }
+const circle1 = new Circle(200, 'green')
+circle1.render()
+
+const circle2 = new Circle(100, 'red')
+circle2.render()
+
+const logo2 = new Logo('https://cond.myfreedom.com.br/assets/img/myfreedom-adm%201.png')
+logo2.init()
+
+
+class Converter {
+    
 }
-
-const frontendDeveloper1 = new FrontendDeveloper('Ainagul', 'Next.js')
-frontendDeveloper1.getInfo()
-frontendDeveloper1.work()
-
-const frontendDeveloper2 = new MiddleFrontendDeveloper('Galymzhan', 'Angular')
-frontendDeveloper2.getInfo()
-frontendDeveloper2.work()
